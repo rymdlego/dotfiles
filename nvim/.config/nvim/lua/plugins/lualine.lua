c23aed039a54f20856df61ba6eb2d314d82d940e
Jakob <jakob@sub.se>
2024-04-28 15:58:25 +0200

    lualine fix for readtime

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,19 +4,8 @@ return {
   config = function()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+    local readtime = require("readtime") -- get wordcount on txt and md files
 
-    -- local colors = {
-    --   blue = "#65D1FF",
-    --   green = "#3EFFDC",
-    --   violet = "#FF61EF",
-    --   yellow = "#FFDA7B",
-    --   red = "#FF4A4A",
-    --   fg = "#c3ccdc",
-    --   bg = "#112638",
-    --   inactive_bg = "#2c3043",
-    -- }
-
-    -- configcatppuccinure lualine with modified theme
     lualine.setup({
       options = {
         theme = "catppuccin",
@@ -29,8 +18,12 @@ return {
             color = { fg = "#ff9e64" },
           },
           { "encoding" },
-          { "fileformat" },
+          -- { "fileformat" },
           { "filetype" },
+          {
+            readtime.get_word_count,
+            cond = readtime.is_text_file,
+          },
         },
       },
     })

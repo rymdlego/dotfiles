95cd432fd31db686a0be04115a24e0d05a422e38
Jakob <jakob@sub.se>
2024-05-08 09:40:03 +0200

    mini-nvim

---
+++ nvim/.config/nvim/lualine.lua
@@ -0,0 +1,31 @@
+-- return {
+--   "nvim-lualine/lualine.nvim",
+--   dependencies = { "nvim-tree/nvim-web-devicons" },
+--   config = function()
+--     local lualine = require("lualine")
+--     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+--     local readtime = require("readtime") -- get wordcount on txt and md files
+--
+--     lualine.setup({
+--       options = {
+--         theme = "catppuccin",
+--       },
+--       sections = {
+--         lualine_x = {
+--           {
+--             lazy_status.updates,
+--             cond = lazy_status.has_updates,
+--             color = { fg = "#ff9e64" },
+--           },
+--           { "encoding" },
+--           -- { "fileformat" },
+--           { "filetype" },
+--           {
+--             readtime.get_word_count,
+--             cond = readtime.is_text_file,
+--           },
+--         },
+--       },
+--     })
+--   end,
+-- }

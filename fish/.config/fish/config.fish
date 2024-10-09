48cf59ea0ded14e7d7be6a8ea2d8daa3615f0cd8
Jakob <jakob@sub.se>
2024-10-09 18:01:40 +0200

    fixed ssh-agent

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -42,4 +42,10 @@ end
 
 
 # SSH Agent
-ssh-agent-setup
+if test -z (pgrep ssh-agent | string collect)
+    eval (ssh-agent -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
+
+ssh-keys-add

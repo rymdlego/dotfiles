a27ccabfaa3101cf272b3cac4a5b3314822abd10
Jakob <jakob@sub.se>
2024-10-09 20:54:30 +0200

    macos settings

--- macos_setup.sh
+++ macos_setup.sh
@@ -10,6 +10,41 @@ if [[ "$(uname)" == "Darwin" ]]; then
     xcode-select --install
   fi
 
+  # Key Repeat
   defaults write -g InitialKeyRepeat -int 15
   defaults write -g KeyRepeat -int 2.0
+
+  # Dock autohide
+  defaults write com.apple.Dock autohide -bool TRUE
+
+  # Finder folders on top
+  defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
+
+  # Finder default view set to List
+  defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
+
+
+  defaults -currentHost write -g "com.apple.keyboard.modifiermapping.0-0-0" -string
+(
+        {
+        HIDKeyboardModifierMappingDst = 30064771300;
+        HIDKeyboardModifierMappingSrc = 30064771129;
+    },
+        {
+        HIDKeyboardModifierMappingDst = 30064771299;
+        HIDKeyboardModifierMappingSrc = 30064771299;
+    },
+        {
+        HIDKeyboardModifierMappingDst = 30064771303;
+        HIDKeyboardModifierMappingSrc = 30064771303;
+    },
+        {
+        HIDKeyboardModifierMappingDst = 30064771302;
+        HIDKeyboardModifierMappingSrc = 30064771302;
+    },
+        {
+        HIDKeyboardModifierMappingDst = 30064771298;
+        HIDKeyboardModifierMappingSrc = 30064771298;
+    }
+)
 fi

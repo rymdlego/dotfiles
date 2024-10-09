78494338d9f1e9c42aa7d0e5ac92911e18cfec3e
Jakob <jakob@sub.se>
2024-10-09 17:33:22 +0200

    cleanup

--- brew_setup.sh
+++ brew_setup.sh
@@ -5,12 +5,3 @@ if command -v brew &>/dev/null; then
 else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-
-LINUX_BREW_PATH="/home/linuxbrew/.linuxbrew/bin"
-MACOS_BREW_PATH="/opt/homebrew/bin"
-
-if [ -d "$LINUX_BREW_PATH" ]; then
-  export BREW_BIN_PATH=$LINUX_BREW_PATH
-else
-  export BREW_BIN_PATH=$MACOS_BREW_PATH
-fi

5f9bc0cc7f52dd4a18431a747fc3c615b050f793
Jakob <jakob@sub.se>
2024-10-10 09:37:53 +0200

    fixed fish ssh environment vars

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -44,8 +44,10 @@ end
 # SSH Agent
 if test -z (pgrep ssh-agent | string collect)
     eval (ssh-agent -c)
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    # set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    # set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    set -x SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -x SSH_AGENT_PID $SSH_AGENT_PID
 end
 
 ssh-keys-add

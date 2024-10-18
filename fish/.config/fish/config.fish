a5ae9784b22df04013f69df350c3b2908eb513a8
Jakob <jakob@sub.se>
2024-10-18 13:56:27 +0200

    fix: fzf theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -38,6 +38,12 @@ set -x BAT_THEME "Catppuccin-frappe"
 set -x NOTES_PATH "$HOME/notes"
 set -x NOTES_DAILIES "Dailies"
 set -x TERM xterm-256color
+set -x FZF_DEFAULT_OPTS "\
+--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
+--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+--color=marker:#babbf1,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284 \
+--color=selected-bg:#51576d \
+--multi"
 
 # Greeting
 set -g fish_greeting

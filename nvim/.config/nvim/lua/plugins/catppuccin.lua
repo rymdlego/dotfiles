6f791f0cd9dbb49a0f91a0f78e23558f753506a4
Jakob <jakob@sub.se>
2024-10-20 10:25:16 +0200

    fix: kubernetes context switcher can now unset current-context

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -6,6 +6,6 @@ return {
     require("catppuccin").setup({
       transparent_background = true,
     })
-    vim.cmd([[colorscheme catppuccin-frappe]])
+    -- vim.cmd([[colorscheme catppuccin-frappe]])
   end,
 }

dba4aa3d4d27ea9d1e0bf87e30a713ea301af3ef
Jakob <jakob@sub.se>
2024-10-22 10:38:08 +0200

    fix: nvim plugin cleanup

--- nvim/.config/nvim/lua/plugins/mini-nvim.lua
+++ nvim/.config/nvim/lua/plugins/mini-nvim.lua
@@ -1,7 +1,10 @@
 return {
-  "echasnovski/mini.statusline",
+  "echasnovski/mini.nvim",
   version = "*",
   config = function()
+    require("mini.diff").setup()
+    require("mini.icons").setup()
+    require("mini.git").setup()
     require("mini.statusline").setup()
   end,
 }

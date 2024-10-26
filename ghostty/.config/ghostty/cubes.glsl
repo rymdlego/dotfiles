a93673fc537d1761dd3637fd0262397f26921eb6
Jakob <jakob@sub.se>
2024-10-26 19:12:39 +0200

    fix lowered brightness on cube shader

--- ghostty/.config/ghostty/cubes.glsl
+++ ghostty/.config/ghostty/cubes.glsl
@@ -1,6 +1,13 @@
+// credits: https://github.com/rymdlego
+
 const vec3 bg = vec3(0.133, 0.153, 0.180);
-const float speed = 0.1485;
-const float offset = 1.53;
+const float speed = 0.2;
+const float cube_size = 1.0;
+const float cube_brightness = 0.5;
+const float cube_rotation_speed = 2.8;
+const float camera_rotation_speed = 0.1;
+
+
 
 mat3 rotationMatrix(vec3 m,float a) {
     m = normalize(m);
@@ -24,7 +31,7 @@ float sphere(vec3 pos, float radius)
 float box(vec3 pos, vec3 size)
 {
     float t = iTime;
-    pos = pos * 0.9 * rotationMatrix(vec3(sin(t/4.0*speed)*10.,cos(t/4.0*speed)*12.,2.7), t*2.4/4.0*speed);
+    pos = pos * 0.9 * rotationMatrix(vec3(sin(t/4.0*speed)*10.,cos(t/4.0*speed)*12.,2.7), t*2.4/4.0*speed*cube_rotation_speed);
     return length(max(abs(pos) - size, 0.0));
 }
 
@@ -33,10 +40,9 @@ float distfunc(vec3 pos)
 {
     float t = iTime;
     
-    float size = 0.45 + 0.25*abs(16.0*sin((t-offset)*speed/4.0));
+    float size = 0.45 + 0.25*abs(16.0*sin(t*speed/4.0));
     // float size = 2.3 + 1.8*tan((t-5.4)*6.549);
-    size = 0.16 * clamp(size, 2.0, 4.0);
-    // size = 0.16 * clamp(size, 2.0, 2.0);
+    size = cube_size * 0.16 * clamp(size, 2.0, 4.0);
 
     //pos = pos * rotationMatrix(vec3(0.,-3.,0.7), 3.3 * mod(t/30.0, 4.0));
     vec3 q = mod(pos, 5.0) - 2.5;
@@ -52,7 +58,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec3 cameraOrigin = vec3(t*1.0*speed, 0.0, 0.0);
     // vec3 cameraOrigin = vec3(t*1.8*speed, 3.0+t*0.02*speed, 0.0);
     vec3 cameraTarget = vec3(t*100., 0.0, 0.0);
-    cameraTarget = vec3(t*20.0,0.0,0.0) * rotationMatrix(vec3(0.0,0.0,1.0), t/5.0*speed);
+    cameraTarget = vec3(t*20.0,0.0,0.0) * rotationMatrix(vec3(0.0,0.0,1.0), t*speed*camera_rotation_speed);
 
     vec3 upDirection = vec3(0.5, 1.0, 0.6);
     
@@ -86,9 +92,9 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
         // Lighting Code
         vec2 eps = vec2(0.0, EPSILON);
         vec3 normal = normalize(vec3(
-          distfunc(pos + eps.yxx) - distfunc(pos - eps.yxx),
-          distfunc(pos + eps.xyx) - distfunc(pos - eps.xyx),
-          distfunc(pos + eps.xxy) - distfunc(pos - eps.xxy)));
+            distfunc(pos + eps.yxx) - distfunc(pos - eps.yxx),
+            distfunc(pos + eps.xyx) - distfunc(pos - eps.xyx),
+            distfunc(pos + eps.xxy) - distfunc(pos - eps.xxy)));
         float diffuse = max(0., dot(-rayDir, normal));
         float specular = pow(diffuse, 32.0);
         vec3 color = vec3(diffuse + specular);
@@ -97,7 +103,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
         color += cubeColor;
         cubes = vec4(color, 1.0) * vec4(1.0 - (totalDist/MAX_DIST));
         // cubes = vec4(bg,1.0) + (vec4(color, 1.0) * vec4(1.0 - (totalDist/MAX_DIST)));
-        cubes = vec4(bg,1.0) + vec4(cubes.rgb*0.02, 0.1);
+        cubes = vec4(bg,1.0) + vec4(cubes.rgb*0.02*cube_brightness, 0.1);
     } 
     else
     {

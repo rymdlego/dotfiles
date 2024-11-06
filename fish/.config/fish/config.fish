2aaf3bbdc64a310cdea6c61bbecf144d0764051d
Jakob <jakob@sub.se>
2024-11-06 17:06:18 +0100

    feat atuin ctrl-t shortcut for dir filtering

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -61,6 +61,17 @@ end
 fish_hybrid_key_bindings
 
 
+# Atuin
+set -gx ATUIN_NOBIND "true"
+atuin init fish | source
+
+# bind to ctrl-r in normal and insert mode, add any other bindings you want here too
+bind \cr _atuin_search
+bind \ct '_atuin_search --filter-mode directory'
+bind -M insert \cr _atuin_search
+bind -M insert \ct '_atuin_search --filter-mode directory'
+
+
 # Greeting
 set -g fish_greeting
 

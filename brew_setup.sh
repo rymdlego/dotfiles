6661c098950c65c2a7df6c5532bff9a4f2d52e58
Jakob <jakob@sub,se>
2024-12-03 09:36:07 +0100

    fix: fixed brew install

--- brew_setup.sh
+++ brew_setup.sh
@@ -5,3 +5,9 @@ if command -v brew &>/dev/null; then
 else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+if test -d $linuxbrew_path
+    eval ($linuxbrew_path/brew shellenv)
+else
+    eval (/opt/homebrew/bin/brew shellenv)
+end

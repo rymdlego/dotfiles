2e209b33fbc409f3104c84e5df61f6784fc9fb8b
Jakob <jakob@sub,se>
2024-12-03 08:51:02 +0100

    fixes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -87,8 +87,8 @@ end
 # ssh-agent
 if not pgrep ssh-agent
     eval (ssh-agent -c)
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    set -x SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -x SSH_AGENT_PID $SSH_AGENT_PID
 end
 
 ssh-keys-add

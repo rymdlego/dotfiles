8b41eb3be8d85b49d67bc764dd94a5aed6e8a181
Jakob <jakob@sub.se>
2025-01-07 10:40:58 +0100

    removed sketchybar

--- sketchybar/.config/sketchybar/plugins/kubernetes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-context=$(sed -n 's/^[[:space:]]*current-context:[[:space:]]*//p' ~/.kube/config | xargs)
-
-if [ "$context" != "" ]; then
-  sketchybar --set "$NAME" icon="⎈" label="$context"
-else
-  sketchybar --set "$NAME" icon="" label=""
-fi
